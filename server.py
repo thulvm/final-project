ea82935224cb6197c834876a70fa9632fc7d2367
Tmaxi Technology <lethanhphong2605@gmail.com>
2023-04-04 16:45:24 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,66 +1,24 @@
-# -*- coding: utf-8 -*-
-import sys
-import copy
-import os
-import unittest
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
 
-curr_filename = os.path.abspath(__file__)
-curr_dir = os.path.dirname(curr_filename)
-script_path = os.path.join(curr_dir, "../script")
-sys.path.append(script_path)
+app = Flask("Web Translator")
 
-from translator import BaicizhanTranslator
-from translator import BingDict
-from translator import GoogleTranslator
-from translator import HaiciDict
-from translator import ICibaTranslator
-from translator import YoudaoTranslator
-from translator import TranslateShell
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    return "Translated text to French"
 
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    return "Translated text to English"
 
-class TestTranslator(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestTranslator, self).__init__(*args, **kwargs)
-
-    @unittest.skip("Expired")
-    def test_baicizhan(self):
-        t = BaicizhanTranslator()
-        r = t.translate("", "", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    @unittest.skip("Skip for GitHub Action")
-    def test_bing(self):
-        t = BingDict()
-        r = t.translate("", "", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    def test_google(self):
-        t = GoogleTranslator()
-        r = t.translate("auto", "zh", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    def test_haici(self):
-        t = HaiciDict()
-        r = t.translate("", "zh", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    @unittest.skip("ciba api was deprecated")
-    def test_iciba(self):
-        t = ICibaTranslator()
-        r = t.translate("", "", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    def test_translate_shell(self):
-        t = TranslateShell()
-        r = t.translate("auto", "zh", "naive")
-        self.maxDiff = None
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
-    def test_youdao(self):
-        t = YoudaoTranslator()
-        r = t.translate("auto", "zh", "naive")
-        self.assertTrue(len(r['paraphrase']) != 0 or len(r['explains']))
-
+@app.route("/")
+def renderIndexPage():
+    # Write the code to render template
 
 if __name__ == "__main__":
-    unittest.main()
+    app.run(host="0.0.0.0", port=8080)
